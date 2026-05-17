8976002be363788f2e78b5d612be92b024d3eb39
Ubuntu <ubuntu@ip-172-31-47-94.eu-west-1.compute.internal>
2026-05-17 17:32:17 +0000

    ddd

--- vars/codeQuality.groovy
+++ vars/codeQuality.groovy
@@ -1,7 +1,7 @@
 def sonarCreateProject(String projectKey) {
         withSonarQubeEnv('SonarQubeScanner') {
             sh """
-                curl -s -u ${env.sonarqube}: \
+                curl -s -u ${env.SONAR_AUTH_TOKEN}: \
                -X POST "${env.SONAR_HOST_URL}/api/projects/create" \
                -d "project=${projectKey}&name=${projectKey}"
             """
